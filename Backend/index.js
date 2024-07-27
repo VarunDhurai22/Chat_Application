e00073aea9852c09e4118a5a38bc0c981454239a
Dvarun <varundhurai19@gmail.com>
2024-07-27 10:39:54 +0530

    error solving

--- Backend/index.js
+++ Backend/index.js
@@ -2,26 +2,33 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import userRoute from "./routes/user.route.js"
-const app = express()
+import cookieParser from "cookie-parser";
+
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
 
 dotenv.config();
-app.use(express.json());
 
+// middleware
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
-const URI = process.env.MONGODB_URI
+const PORT = process.env.PORT || 3001;
+const URI = process.env.MONGODB_URI;
 
 try {
-  mongoose.connect(URI);
-  (console.log("connected to MongoDB"));
-
+    mongoose.connect(URI);
+    console.log("Connected to MongoDB");
 } catch (error) {
-  console.log(error);
+    console.log(error);
 }
-app.use("/user", userRoute);
-const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+//routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
+
+server.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
 });
--- Frontend/src/context/Authprovider.jsx
+++ Frontend/src/context/Authprovider.jsx
@@ -1,16 +1,19 @@
-import React, { createContext, useContext, useState } from 'react'
-import Cookies from "js-cookie"
-export const AuthContext=createContext()
-const Authprovider = ({childeren}) => {
-  const initialUserState=Cookies.get("jwt")||localStorage.getItem("ChatApp");
+import React, { createContext, useContext, useState } from "react";
+import Cookies from "js-cookie";
+export const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+  const initialUserState =
+    Cookies.get("jwt") || localStorage.getItem("ChatApp");
 
-  const [authUser, setAuthUser] = useState(initialUserState? JSON.parse(initialUserState) : undefined);
-
+  // parse the user data and storing in state.
+  const [authUser, setAuthUser] = useState(
+    initialUserState ? JSON.parse(initialUserState) : undefined
+  );
   return (
     <AuthContext.Provider value={[authUser, setAuthUser]}>
-        {childeren}
+      {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
-export default useAuth=()=> useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);
